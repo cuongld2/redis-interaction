c0609a9e22108ab1c222c8cfbe07748da3187f59
cuongld2 <ledinhcuong99@gmail.com>
2019-12-23 17:45:13 +0700

    Modify decode response support utf8

--- test-scripts/test_redis_cluster.py
+++ test-scripts/test_redis_cluster.py
@@ -1,12 +1,17 @@
 def test_masters_slaves():
     from rediscluster import RedisCluster
-    nodes = [{"host": "redis-cluster-coccoc-music-0.browser-dev.svc.cluster.local",
+    # for node in [0, 1, 2, 3, 4, 5]:
+    nodes = [{"host": f"redis-cluster-coccoc-music-0.browser-dev.svc.cluster.local",
               "port": "6379"}]
-    r = RedisCluster(startup_nodes=nodes, password="dga4DD26V", skip_full_coverage_check=True)
-    all_keys = r.keys("*")
-    for each_key in all_keys:
-        print(str(r.type(each_key)).split('b')[1])
-        if 'string' in (str(r.type(each_key)).split('b')[1]):
-            print(r.get(each_key))
-        else:
-            print(r.smembers(each_key))
+    r = RedisCluster(startup_nodes=nodes, password="dga4DD26V", skip_full_coverage_check=True, decode_responses=u'utf-8')
+    # all_keys = r.keys("*")
+    # if 'b"coccoc-music-api:ALL_SOURCES"' in all_keys:
+    #     r.get('b"coccoc-music-api:ALL_SOURCES"')
+    # for each_key in all_keys:
+    #     if 'string' in (str(r.type(each_key)).split('b')[1]):
+    #         if "coccoc-music-api:ALL_SOURCES" in str(each_key):
+    #             print(r.get(each_key))
+    # print(f"\nKeys are : {all_keys}")
+    print(r.get('coccoc-music-api:ALL_CATEGORIES'))
+
+
